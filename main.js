dd13c84813c0065157cc220a8660c8108ded60f2
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-07-22 06:18:54 -0500

    Finished calculator

--- main.js
+++ main.js
@@ -1,46 +1,67 @@
-function add(a,b){
-    return a + b;
-}
-function subtract(a,b){
-    return a - b;
-}
-
-function multiply(a,b){
-    return a * b;
-}
-
-function divide(a,b){
-    return a / b;
-}
-function divisble(a,b){
-    return a % b;
-}
-
-let a = '';
-let b = '';
-let op = '';
-
-function calculate(a, b, op){
-    if(op === '+'){
-        return add(a,b);
-    } 
-    else if(op === '-'){
-        return subtract(a,b);
+function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function calculateResult() {
+    const display = document.getElementById('display');
+    const expression = display.value;
+  
+    try {
+      const result = evalExpression(expression);
+      display.value = result;
+    } catch (error) {
+      display.value = 'Error';
     }
-    else if(op === '×' || op === 'x' || op === '*'){
-        return multiply(a,b);
+  }
+  
+  function evalExpression(expression) {
+    // Using regular expression to split the expression and handle spaces
+    const tokens = expression.match(/\d+|\+|\-|\*|\//g) || [];
+    const numbers = [];
+    const operators = [];
+  
+    function applyOperator(operator) {
+      const b = numbers.pop();
+      const a = numbers.pop();
+      switch (operator) {
+        case '+':
+          numbers.push(a + b);
+          break;
+        case '-':
+          numbers.push(a - b);
+          break;
+        case '*':
+          numbers.push(a * b);
+          break;
+        case '/':
+          numbers.push(a / b);
+          break;
+      }
     }
-    else if(op === '÷' || op === '/'){
-        return subtract(a,b);
+  
+    tokens.forEach(token => {
+      if (!isNaN(token)) {
+        numbers.push(parseFloat(token));
+      } else if (token === '+' || token === '-') {
+        while (operators.length > 0 && (operators[operators.length - 1] === '+' || operators[operators.length - 1] === '-')) {
+          applyOperator(operators.pop());
+        }
+        operators.push(token);
+      } else if (token === '*' || token === '/') {
+        while (operators.length > 0 && (operators[operators.length - 1] === '*' || operators[operators.length - 1] === '/')) {
+          applyOperator(operators.pop());
+        }
+        operators.push(token);
+      }
+    });
+  
+    while (operators.length > 0) {
+      applyOperator(operators.pop());
     }
-    else if(op === '%'){
-        return divide(a,b);
-    }
-}
-
-const buttons = document.querySelectorAll('.btn');
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-  });
-});
+  
+    return numbers[0];
+  }
